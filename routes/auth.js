20cef28762e22befd1055ec15668bbe626aba254
Steven James McDonald <stmc6816@colorado.edu>
2019-04-21 15:34:26 -0600

    Callbacks

--- routes/auth.js
+++ routes/auth.js
@@ -18,16 +18,13 @@ router.get('/login', passport.authenticate('auth0', {
 // Perform the final stage of authentication and redirect to previously requested URL or '/user'
 router.get('/callback', function (req, res, next) {
   passport.authenticate('auth0', function (err, user, info) {
-    userProfile = user;
-    //console.log(user);
-    console.log("Callback initiated");
     if (err) { return next(err); }
     if (!user) { return res.redirect('/login'); }
     req.logIn(user, function (err) {
       if (err) { return next(err); }
       const returnTo = req.session.returnTo;
       delete req.session.returnTo;
-      res.redirect('/user');
+      res.redirect(returnTo || '/user');
     });
   })(req, res, next);
 });
