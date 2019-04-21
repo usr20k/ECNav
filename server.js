367a95ccf3e497a3d0a6775b5e972791d3217ee4
Steven James McDonald <stmc6816@colorado.edu>
2019-04-21 16:10:02 -0600

    asdsa

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ var strategy = new Auth0Strategy(
     domain: process.env.AUTH0_DOMAIN,
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+    callbackURL: 'http://localhost:3000/callback'
   },
 
   function (accessToken, refreshToken, extraParams, profile, done) {
