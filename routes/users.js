df24f2204e32ac3aff6b3674440a53b8392dc484
Steven James McDonald <stmc6816@colorado.edu>
2019-04-21 18:27:13 -0600

    test

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var secured = require('../lib/middleware/secured');
 var router = express.Router();
-
+console.log("User redirect js loaded.");
 /* GET user profile. */
 router.get('/user', secured(), function (req, res, next) {
   const { _raw, _json, ...userProfile } = req.user;
