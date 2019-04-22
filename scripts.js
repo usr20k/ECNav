cf28fdec5b5265245d130463206c79c4dce68b8b
RobbieTennant <46974558+RobbieTennant@users.noreply.github.com>
2019-04-22 13:41:52 -0600

    updated scripts.js

--- scripts.js
+++ scripts.js
@@ -19,42 +19,44 @@
       document.getElementsByClassName("speech-bubble")[0].style.top = (y-document.getElementsByClassName("speech-bubble")[0].offsetHeight+15) + "px";
       document.getElementsByClassName("speech-bubble")[0].style.left = (x-document.getElementsByClassName("speech-bubble")[0].offsetWidth+20) + "px";
       
-      while(zoomLevel!=-8){
+      while(zoomLevel!=0){
         zoomOut();
       }
       document.documentElement.scrollLeft = x * Math.pow(8/7,zoomLevel) - window.innerWidth/2;
       document.documentElement.scrollTop = y * Math.pow(8/7,zoomLevel) - window.innerHeight/2;
+      
 	  }
     function displayInfo(roomNum){
       document.getElementsByClassName("speech-bubble")[0].innerHTML = roomNum;
     }
 
 	function zoomIn(){
-    zoomLevel++;
-		realWidth = parseFloat(realWidth* (8/7));
-    document.getElementById("map").width = realWidth;
-		var pixelCords = document.getElementsByClassName("speech-bubble")[0];
-    document.getElementsByClassName("speech-bubble")[0].style.top = (((pixelCords.offsetTop-pixelCords.offsetHeight+10)*(8/7)) + (pixelCords.offsetHeight)+1) + "px";
-    document.getElementsByClassName("speech-bubble")[0].style.left = ((((pixelCords.offsetLeft)-pixelCords.offsetWidth+20)*(8/7)) + (pixelCords.offsetWidth)+3) + "px";
-    document.documentElement.scrollTop  = ((document.documentElement.scrollTop+(window.innerHeight/2)) * 8/7) - (window.innerHeight/2);
-    document.documentElement.scrollLeft = ((document.documentElement.scrollLeft +(window.innerWidth/2)) * 8/7) - (window.innerWidth/2);
+    if(zoomLevel<-2){
+      zoomLevel++;
+  		realWidth = parseFloat(realWidth* (8/7));
+      document.getElementById("map").width = realWidth;
+  		var pixelCords = document.getElementsByClassName("speech-bubble")[0];
+      document.getElementsByClassName("speech-bubble")[0].style.top = (((pixelCords.offsetTop-pixelCords.offsetHeight+10)*(8/7)) + (pixelCords.offsetHeight)+5) + "px";
+      document.getElementsByClassName("speech-bubble")[0].style.left = ((((pixelCords.offsetLeft)-pixelCords.offsetWidth+20)*(8/7)) + (pixelCords.offsetWidth)+4) + "px";
+      document.documentElement.scrollTop  = ((document.documentElement.scrollTop+(window.innerHeight/2)) * 8/7) - (window.innerHeight/2);
+      document.documentElement.scrollLeft = ((document.documentElement.scrollLeft +(window.innerWidth/2)) * 8/7) - (window.innerWidth/2);
+    }
   }
 	function zoomOut(){
-    zoomLevel--;
-    realWidth = parseFloat(realWidth* (7/8));
-		document.getElementById("map").width =realWidth.toFixed(0);
-		var pixelCords = document.getElementsByClassName("speech-bubble")[0];
-    document.getElementsByClassName("speech-bubble")[0].style.top = (((pixelCords.offsetTop-pixelCords.offsetHeight-10)*(7/8)) + (pixelCords.offsetHeight))-3+ "px";
-    document.getElementsByClassName("speech-bubble")[0].style.left = ((((pixelCords.offsetLeft)-pixelCords.offsetWidth-20)*(7/8)) + (pixelCords.offsetWidth))-6 + "px";
-    document.documentElement.scrollTop  = (document.documentElement.scrollTop+(window.innerHeight/2) * 7/8) - window.innerHeight/2;
-    document.documentElement.scrollLeft = (document.documentElement.scrollLeft +(window.innerWidth/2) * 7/8) - window.innerWidth/2;
+    if(zoomLevel>-13){
+      zoomLevel--;
+      realWidth = parseFloat(realWidth* (7/8));
+  		document.getElementById("map").width =realWidth.toFixed(0);
+  		var pixelCords = document.getElementsByClassName("speech-bubble")[0];
+      document.getElementsByClassName("speech-bubble")[0].style.top = (((pixelCords.offsetTop-pixelCords.offsetHeight-10)*(7/8)) + (pixelCords.offsetHeight)-6)+ "px";
+      document.getElementsByClassName("speech-bubble")[0].style.left = ((((pixelCords.offsetLeft)-pixelCords.offsetWidth-20)*(7/8)) + (pixelCords.offsetWidth)-6) + "px";
+      document.documentElement.scrollTop  = ((document.documentElement.scrollTop+(window.innerHeight/2)) * 7/8) - (window.innerHeight/2);
+      document.documentElement.scrollLeft = ((document.documentElement.scrollLeft +(window.innerWidth/2)) * 7/8) - (window.innerWidth/2);
+    }
 	}
 
 
   function status(){
-      //alert("LeftScroll: " + document.documentElement.scrollLeft + "\nTopScroll: " + document.documentElement.scrollTop + "\nCenter: "+ (document.documentElement.scrollLeft+(window.innerWidth/2))+","+(document.documentElement.scrollTop+(window.innerHeight/2)) + "\nWidth"+window.innerWidth);
-  		//var pixelCords = document.getElementsByClassName("speech-bubble")[0];
-  		//alert("Left: " + pixelCords.offsetLeft +"\nTop: " +pixelCords.offsetTop +"\nWidth: "+ document.getElementById("map").width + "\n Height: " + document.getElementById("map").height);
       loadInfo("TestRoom",4575,3870,"map.jpg");
 	}
  function loadInfo(roomNum,x,y,img){
